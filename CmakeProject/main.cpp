eaf76d52e22475fd7f50b04621f9803d8b77bb96
Nubox <sjhbelieve@gmail.com>
2025-08-10 12:22:11 +0900

    Update

--- CmakeProject/main.cpp
+++ CmakeProject/main.cpp
@@ -1,34 +1,36 @@
-#include <iostream> 
-#include <vector>
-#define INIT ios_base::sync_with_stdio(0); cin.tie(0);
-using namespace std;
+#include <iostream>
 
-int N, R, Q;
-vector<vector<int>> matrix;
-
-int main()
+int w = 4, h = 4;
+int Chkover(int i, int j)
 {
-    cin >> N >> R >> Q;
+    return i >=0 && i < w && j >= 0  && j < h ? 1 : 0;
+}
+int main() 
+{
+    int field[4][4] = {{0,1,0,1},{0,0,0,1},{1,1,1,0},{0,1,1,1}};
+    int mines[4][4] = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
 
-    matrix.resize(N+1);
-    for(int i = 0; i < N+1; ++i)
-        matrix[i].resize(N+1);
-    
-
-    for(int i = 1; i <= N; ++i)
+    for(int y = 0; y < h; ++y)
     {
-        int temp1, temp2;
-        cin >> temp1 >> temp2;
-        matrix[temp1][temp2] = 1;
-        matrix[temp2][temp1] = 1;
-    }
-    
-    for(int i = 0; i < Q; ++i)
-    {
-        int temp;
-        cin >> temp;
+        for(int x = 0; x < w; ++x)
+        {
+            if(field[y][x] == 0) continue;
+            for(int j = y-1; j <= y+1; ++j)
+            {
+                for(int i = x-1; i <= x+1; ++i)
+                    if(Chkover(j,i) == 1) ++mines[j][i];
+            }
+        }
     }
 
+    for(int y = 0; y < h; ++y)
+    {
+        for(int x = 0; x < w; ++x)
+        {
+            std::cout << mines[y][x] << " ";
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
